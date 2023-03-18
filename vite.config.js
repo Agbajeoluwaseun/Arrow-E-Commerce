9a6e8fe91cf91e275b0abc0c0908f1daa6703b1f
Agbaje Emmanuel Oluwaseun <106745361+Agbajeoluwaseun@users.noreply.github.com>
2023-03-18 13:02:22 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
 export default defineConfig({
   plugins: [react()],
-    build: {
-        chunkSizeWarningLimit: 100000000
-    },
 })
+
+// export default defineConfig({
+//   plugins: [react()],
+//     build: {
+//         chunkSizeWarningLimit: 100000000
+//     },
+// })
